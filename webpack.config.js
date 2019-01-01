1dea2868793151deccff60df5bb879bd68d2d5c8
Noam Bachmann <noam@helou.ch>
2019-01-01 18:52:03 +0100

    working webpack

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+module.exports = [{
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    }
+}, {
+    entry: './src/scss/style.scss',
+    output: {
+        // This is necessary for webpack to compile
+        // But we never use style-bundle.js
+        path: path.resolve('dist/'),
+        filename: 'style-bundle.js',
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                    loader: 'file-loader',
+                    options: {
+                        path: path.resolve('generated/'),
+                        name: 'style.css',
+                    },
+                },
+                /* { loader: 'css-loader' },*/
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        includePaths: ['./node_modules'],
+                        implementation: require("sass")
+                    }
+                }
+            ]
+        }]
+    },
+}, {
+    entry: "./src/js/index.js",
+    output: {
+        path: path.resolve("dist/"),
+        filename: "index.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }]
+    },
+    devtool: 'source-map',
+}];

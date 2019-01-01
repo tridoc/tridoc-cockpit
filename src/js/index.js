66e93b58f39d1fdd5aa251edb46ae0102c3390d0
Noam Bachmann <noam@helou.ch>
2019-01-01 21:58:49 +0100

    fillout of title

--- src/js/index.js
+++ src/js/index.js
@@ -22,14 +22,14 @@ if (storage.getItem("password")) {
 
 let server = new Server(urlInput.value, usernameInput.value, passwordInput.value);
 
-const count = () => {
-    server.countDocuments("", "", "")
-        .then(count => {
-            const counter = document.getElementById("counter");
-            counter.innerHTML = count;
-        });
+function fillout() {
+    let documentTitle = this.innerHTML;
+    let id = this.getAttribute("data-document-id");
+    if (documentTitle != id) {
+        let title = document.getElementById("current-title")
+        title.innerHTML = documentTitle;
+    }
 }
-
 const searchDocuments = (page) => {
     let query = "";
     let dest = document.getElementById("document-list");
@@ -38,31 +38,33 @@ const searchDocuments = (page) => {
     let tagsQuery = "";
     if (tags.length > 0) {
         encodeURIComponent(tags);
-        tagsQuery = "&tag=" + tags.replace(/\s?,\s?/,"&tag=");
+        tagsQuery = "&tag=" + tags.replace(/\s?,\s?/, "&tag=");
     }
     let notTagsQuery = "";
     if (notTags.length > 0) {
         encodeURIComponent(notTags);
-        notTagsQuery = "&nottag=" + notTags.replace(/\s?,\s?/,"&nottag=");
+        notTagsQuery = "&nottag=" + notTags.replace(/\s?,\s?/, "&nottag=");
     }
     console.log(tagsQuery + notTagsQuery);
-    if (isNaN(page)) { page = 0 }
+    if (isNaN(page)) {
+        page = 0
+    }
     let limit = ((storage.getItem("limit") > 0) ? storage.getItem("limit") : '');
-    let offset = page*limit;
+    let offset = page * limit;
     let to = 0;
 
-    server.getDocuments(query,tagsQuery,notTagsQuery,limit,offset).then(array => {
+    server.getDocuments(query, tagsQuery, notTagsQuery, limit, offset).then(array => {
         let list = '';
         if (array.error) {
             dest.innerHTML = e;
         } else if (array.length > 0) {
             array.forEach(a => {
                 let label = a.title ? a.title : a.identifier;
-                list = list + `<button class='list-item list-item-flexible' data-document-id="${a.identifier}">${label}</button>`;
+                list = list + `<button class='list-item list-item-flexible fillout-button' data-document-id="${a.identifier}">${label}</button>`;
             });
             dest.innerHTML = list;
             if (list != "") {
-                document.querySelectorAll(".document-edit").forEach(element => element.addEventListener("click", fillout));
+                document.querySelectorAll(".fillout-button").forEach(element => element.addEventListener("click", fillout));
             }
         } else {
             let label1 = query ? " Nothing Found" : " Documents will appear here";
@@ -78,8 +80,7 @@ const searchDocuments = (page) => {
         }
         document.querySelectorAll(".page-switch").forEach(element => element.addEventListener("click", () => {
             searchDocuments(element.getAttribute("data-pagination-target"));
-        } ));
-        getTags();
+        }));
     }).catch(e => {
         console.log(e);
     });
@@ -100,9 +101,8 @@ const saveServer = () => {
 
 /* -- */
 
-document.querySelector("#server-save").addEventListener("click", saveServer);
+document.getElementById("server-save").addEventListener("click", saveServer);
 
 /* -- */
 
-count();
 searchDocuments();

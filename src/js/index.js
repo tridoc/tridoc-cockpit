18400f5b0af16c613160a56b3ffd8ac390138c4f
Noam Bachmann <noam@helou.ch>
2019-01-04 18:24:03 +0100

    displaying tag-type icon in tag list

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,9 @@ const saveIcon = '<svg viewBox="0 0 24 24"><path d="M15,9H5V5H15M12,19A3,3 0 0,1
 const editIcon = '<svg viewBox="0 0 24 24"><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>';
 const addTagIcon = '<svg viewBox="0 0 24 24"><path d="M21.41,11.58L12.41,2.58C12.04,2.21 11.53,2 11,2H4A2,2 0 0,0 2,4V11C2,11.53 2.21,12.04 2.59,12.41L3,12.81C3.9,12.27 4.94,12 6,12A6,6 0 0,1 12,18C12,19.06 11.72,20.09 11.18,21L11.58,21.4C11.95,21.78 12.47,22 13,22C13.53,22 14.04,21.79 14.41,21.41L21.41,14.41C21.79,14.04 22,13.53 22,13C22,12.47 21.79,11.96 21.41,11.58M5.5,7A1.5,1.5 0 0,1 4,5.5A1.5,1.5 0 0,1 5.5,4A1.5,1.5 0 0,1 7,5.5A1.5,1.5 0 0,1 5.5,7M10,19H7V22H5V19H2V17H5V14H7V17H10V19Z"/></svg>';
 
+const dateIcon = '<svg viewBox="0 0 24 24"><path d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3H18V1"/></svg>'
+const numberIcon = '<svg viewBox="0 0 24 24"> <path d="M4,4H20A2,2 0 0,1 22,6V18A2,2 0 0,1 20,20H4A2,2 0 0,1 2,18V6A2,2 0 0,1 4,4M4,6V18H11V6H4M20,18V6H18.76C19,6.54 18.95,7.07 18.95,7.13C18.88,7.8 18.41,8.5 18.24,8.75L15.91,11.3L19.23,11.28L19.24,12.5L14.04,12.47L14,11.47C14,11.47 17.05,8.24 17.2,7.95C17.34,7.67 17.91,6 16.5,6C15.27,6.05 15.41,7.3 15.41,7.3L13.87,7.31C13.87,7.31 13.88,6.65 14.25,6H13V18H15.58L15.57,17.14L16.54,17.13C16.54,17.13 17.45,16.97 17.46,16.08C17.5,15.08 16.65,15.08 16.5,15.08C16.37,15.08 15.43,15.13 15.43,15.95H13.91C13.91,15.95 13.95,13.89 16.5,13.89C19.1,13.89 18.96,15.91 18.96,15.91C18.96,15.91 19,17.16 17.85,17.63L18.37,18H20M8.92,16H7.42V10.2L5.62,10.76V9.53L8.76,8.41H8.92V16Z"/></svg>'
+
 const urlInput = document.getElementById('server-url');
 const usernameInput = document.getElementById('server-username');
 const passwordInput = document.getElementById('server-password');
@@ -163,24 +166,22 @@ const getTagList = () => {
             })
             array.forEach(a => {
                 let type = 'simple';
-                let icon = 'mi-tag';
                 let value;
                 if (a.parameter) {
                     value = a.parameter.value;
                     if (a.parameter.type == 'http://www.w3.org/2001/XMLSchema#decimal') {
                         type = 'decimal';
-                        icon = 'mi-num';
                     } else if (a.parameter.type == 'http://www.w3.org/2001/XMLSchema#date') {
                         type = 'date';
-                        icon = 'mi-cal';
                     }
                 }
                 const newTag = document.createElement("li");
                 newTag.classList = 'tag list-tag';
                 newTag.setAttribute('data-tag-type', type);
                 newTag.setAttribute('data-tag-label', a.label);
+                const icon = type === 'date' ? dateIcon : type === 'decimal' ? numberIcon : '';
                 const valueIndicator = value ? '<span class="tag-value">' + value + '</span>' : '';
-                newTag.innerHTML = `<span class="tag-text">${a.label}</span>${valueIndicator}`;
+                newTag.innerHTML = `<div class="tag-icon">${icon}</div><span class="tag-text">${a.label}</span>${valueIndicator}`;
                 tagList.appendChild(newTag)
             });
         } else {

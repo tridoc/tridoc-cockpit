eb8325176852c1821930f66eb1447db2b818ca0a
Noam Bachmann <noam@helou.ch>
2019-01-04 13:28:46 +0100

    title editable

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,9 @@ import pdfjsLib from 'pdfjs-dist';
 var pdfjsLib = window['pdfjs-dist/build/pdf'];
 pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';*/
 
+const saveIcon = '<svg viewBox="0 0 24 24"><path d="M15,9H5V5H15M12,19A3,3 0 0,1 9,16A3,3 0 0,1 12,13A3,3 0 0,1 15,16A3,3 0 0,1 12,19M17,3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V7L17,3Z"/></svg>';
+const editIcon = '<svg viewBox="0 0 24 24"><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>';
+
 const urlInput = document.getElementById('server-url');
 const usernameInput = document.getElementById('server-username');
 const passwordInput = document.getElementById('server-password');
@@ -13,10 +16,15 @@ const searchInput = document.getElementById('search-documents');
 const searchLoader = document.createElement('div');
 searchLoader.classList.add('loader');
 
+const main = document.getElementById('main');
+
 const documentContainer = document.getElementById('document');
 const documentText = document.getElementById('pdf-text');
 const documentLoader = document.createElement('div');
 documentLoader.classList.add('loader');
+
+const currentTitle = document.getElementById('current-title');
+const editTitleButton = document.getElementById('edit-title');
 
 const storage = localStorage;
 
@@ -36,7 +44,7 @@ if (storage.getItem('password')) {
 
 let server = new Server(urlInput.value, usernameInput.value, passwordInput.value);
 
-const generateError = (e) => {
+const generateError = e => {
     const errorElem = document.createElement('div');
     errorElem.classList = 'error';
     errorElem.textContent = e.status ? `Error ${e.status}: ${e.message ? e.message : e}` : e;
@@ -45,7 +53,7 @@ const generateError = (e) => {
 
 const renderPages = (pdf, pageNumber = 1, max = -1, container = documentContainer) => {
     const nopreview = container === documentContainer;
-    pdf.getPage(pageNumber).then(function (page) {
+    pdf.getPage(pageNumber).then(page => {
         console.log(`Page ${pageNumber} loaded`);
 
         const canvas = document.createElement('canvas');
@@ -73,7 +81,7 @@ const renderPages = (pdf, pageNumber = 1, max = -1, container = documentContaine
             viewport: scaledViewport
         };
         var renderTask = page.render(renderContext);
-        renderTask.promise.then(function () {
+        renderTask.promise.then(() => {
             if (documentLoader.parentNode && nopreview) documentLoader.parentNode.removeChild(documentLoader);
             container.appendChild(canvas);
             console.log(`Page ${pageNumber} of ${max > -1 ? max : pdf.numPages} rendered`);
@@ -97,10 +105,10 @@ const render = (id) => {
             'Authorization': server.headers.get('Authorization')
         }
     });
-    loadingTask.promise.then(function (pdf) {
+    loadingTask.promise.then(pdf => {
         console.log('PDF loaded');
         renderPages(pdf);
-    }, function (reason) {
+    }, reason => {
         if (documentLoader.parentNode) documentLoader.parentNode.removeChild(documentLoader);
         documentContainer.appendChild(generateError(reason));
     });
@@ -116,28 +124,30 @@ const renderPreview = (element) => {
             'Authorization': server.headers.get('Authorization')
         }
     });
-    loadingTask.promise.then(function (pdf) {
+    loadingTask.promise.then(pdf => {
         console.log('PDF loaded');
         renderPages(pdf, 1, 1, element);
-    }, function (reason) {
+    }, reason => {
         // PDF loading error
         console.error(reason);
     });
 }
 
-function getTags() {
+const getTags = () => {
     document.querySelectorAll('.s__th').forEach((dest) => {
         const id = dest.closest('.s__id').getAttribute('data-document-id');
         if (!id) return;
-        const loader = document.createElement("div")
-        loader.classList = 'loader loader-small inline'
-        dest.appendChild(loader);
+        if (!dest.getElementsByClassName("loader")[0]) {
+            const loader = document.createElement("div")
+            loader.classList = 'loader loader-small inline'
+            dest.appendChild(loader);
+        }
         server.getTags(id).then(array => {
             let list = '';
             if (array.error) {
                 dest.innerHTML = 'Error: ' + array.error;
             } else if (array.length > 0) {
-                array.sort(function (a, b) {
+                array.sort((a, b) => {
                     return a.label.localeCompare(b.label);
                 })
                 array.forEach(a => {
@@ -175,29 +185,52 @@ function getTags() {
     });
 }
 
-function fillout() {
-    console.log('Fillout');
-    let documentTitle = this.getElementsByClassName('s__t')[0].innerHTML;
-    let id = this.getAttribute('data-document-id');
-    let title = document.getElementById('current-title');
-    title.innerHTML = documentTitle === id ? `<i>Untitled Document <code>${id}<code></i>` : documentTitle;
-    document.getElementsByClassName('main')[0].setAttribute('data-document-id', id);
+const resetEditButton = () => {
+    currentTitle.contentEditable = false;
+    editTitleButton.innerHTML = editIcon;
+    editTitleButton.removeEventListener('click', saveTitle);
+    editTitleButton.addEventListener('click', editTitle);
+}
+
+const saveTitle = () => {
+    resetEditButton();
+    const id = main.getAttribute('data-document-id');
+    const title = currentTitle.innerHTML;
+    server.setDocumentTitle(id, title).then(r => searchDocuments());
+}
+const editTitle = () => {
+    currentTitle.contentEditable = true;
+    currentTitle.innerHTML = main.getAttribute('data-document-title') || '';
+    editTitleButton.innerHTML = saveIcon;
+    editTitleButton.removeEventListener('click', editTitle);
+    editTitleButton.addEventListener('click', saveTitle);
+}
+
+const fillout = (element) => {
+    resetEditButton();
+    const titleElement = element.getElementsByClassName('s__t')[0];
+    let documentTitle = titleElement.getAttribute('data-document-title') || titleElement.innerHTML;
+    let id = element.getAttribute('data-document-id');
+    currentTitle.innerHTML = documentTitle === id ? `<i>Untitled Document <code>${id}<code></i>` : documentTitle;
+    main.setAttribute('data-document-id', id);
+    main.setAttribute('data-document-title', titleElement.getAttribute('data-document-title') || '');
     getTags();
     render(id);
 }
+
+const regx = (re, string, join = ', ') => {
+    const match = re.exec(string);
+    return match ? join + match[1] + regx(re, string, join) : '';
+}
+
 const searchDocuments = (page) => {
     let dest = document.getElementById('document-list');
     dest.innerHTML = '';
     dest.appendChild(searchLoader);
-
-    function regx(re, string, join = ', ') {
-        const match = re.exec(string);
-        return match ? join + match[1] + regx(re, string, join) : '';
-    }
     const value = ' ' + searchInput.value;
     const tagsregex = /[^!#\\\b]\B#([^\s"',;:]+?(?=\s|$))/g;
     const nottagsregex = /[^#\\]\B!#([^\s"',;:]+?(?=\s|$))/g;
-    const query = value ? encodeURIComponent(value.replace(tagsregex, '').replace(nottagsregex, '').replace(/^ /,'').replace(/\\/,'')) : '';
+    const query = value ? encodeURIComponent(value.replace(tagsregex, '').replace(nottagsregex, '').replace(/^ /, '').replace(/\\/, '')) : '';
     const tags = regx(tagsregex, value, '&tag=');
     let notTags = regx(nottagsregex, value, '&nottag=');
     let tagsQuery = tags ? tags : '';
@@ -219,6 +252,7 @@ const searchDocuments = (page) => {
                 const documentListEntry = document.createElement('button');
                 documentListEntry.classList = 'list-item list-item-flexible fillout-button s__id';
                 documentListEntry.setAttribute('data-document-id', a.identifier);
+                documentListEntry.setAttribute('data-document-title', a.title)
                 const title = document.createElement('h2');
                 title.classList = 's__t';
                 title.innerHTML = label;
@@ -226,13 +260,14 @@ const searchDocuments = (page) => {
                 const tagsHere = document.createElement('div');
                 tagsHere.classList = 's__th';
                 documentListEntry.appendChild(tagsHere);
-                documentListEntry.addEventListener('click', fillout);
+                documentListEntry.addEventListener('click', filloutFromEvent);
                 //renderPreview(documentListEntry); currently crashes browser
                 searchLoader.parentNode.removeChild(searchLoader);
                 dest.appendChild(documentListEntry);
                 dest.appendChild(searchLoader);
             });
             searchLoader.parentNode.removeChild(searchLoader);
+            fillout(dest.firstChild);
         } else {
             let label1 = query ? " Nothing Found" : " Documents will appear here";
             let label2 = query ? "You can try another query" : "Upload something";
@@ -273,6 +308,12 @@ saveButton.addEventListener('click', saveServer);
 searchInput.addEventListener('keydown', e => {
     if (e.key === 'Enter') searchDocuments()
 });
+
+function filloutFromEvent() {
+    fillout(this);
+}
+
+editTitleButton.addEventListener('click', editTitle)
 
 /* - ON LOAD - */
 

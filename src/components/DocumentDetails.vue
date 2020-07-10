c91706c561d2d1e3fada4ab3fce415d5a633758c
nleanba <noam@factsmission.com>
2020-07-10 21:57:27 +0200

    added pdfvuer

--- src/components/DocumentDetails.vue
+++ src/components/DocumentDetails.vue
@@ -1,5 +1,5 @@
 <template>
-<v-dialog v-model="show" scrollable :fullscreen="true /*$vuetify.breakpoint.smAndDown*/">
+<v-dialog v-model="show" scrollable fullscreen>
     <template v-slot:activator="{ on }">
       <v-btn
         class="ma-1"
@@ -12,71 +12,119 @@
         <span :hidden="$vuetify.breakpoint.sm">Edit</span>
       </v-btn>
     </template>
-    <v-card :tile="true /*$vuetify.breakpoint.smAndDown*/">
-      <v-toolbar dark color="primary" flat elevate-on-scroll v-if="true /*$vuetify.breakpoint.smAndDown*/">
+    <v-card tile>
+      <v-toolbar dark color="primary" flat elevate-on-scroll>
         <v-btn icon dark @click="show = false">
           <v-icon>mdi-close</v-icon>
         </v-btn>
         <v-toolbar-title>{{ meta.title || meta.identifier }}</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-toolbar-items>
-          <v-btn dark text @click="show = false">Close</v-btn>
-        </v-toolbar-items>
-      </v-toolbar>
-      <v-card-title v-if="false /*$vuetify.breakpoint.mdAndUp*/">
-        <span class="headline">{{ meta.title || meta.identifier }}</span>
-      </v-card-title>
-      <v-card-text>
-        <v-chip-group>
-          <v-chip
-            v-for="tag in meta.tags"
-            :key="tag.label"
-            label
+        <!--<v-toolbar-items>-->
+          <v-btn
+            :icon="$vuetify.breakpoint.smAndDown"
+            text
+            dark
+            @click="openDocument(meta.identifier)"
           >
-            <v-icon v-if="tag.label === '..'">mdi-sync</v-icon>
-            <span v-else>{{ tag.label }}</span>
-            <v-divider class="mx-3" vertical v-if="tag.parameter"></v-divider>
-            <strong v-if="tag.parameter">{{ tag.parameter.type === 'http://www.w3.org/2001/XMLSchema#decimal' ? tag.parameter.value : calculateDatestamp(tag.parameter.value) }}</strong>
-          </v-chip>
-        </v-chip-group>
-        <v-progress-linear
-          v-if="loading"
-          indeterminate
-          rounded
-          height="6"
-        />
-        <pdf
-          :src="pdfsrc()"
-          v-for="i in numPages"
-          :key="i"
-          :id="i"
-          :page="i"
-          :scale.sync="scale"
-          resize
-          style="width:100%;margin:20px auto;"
-          @loading="(b) => loading = b"
-        />
+            <v-icon :left="!$vuetify.breakpoint.smAndDown">mdi-open-in-new</v-icon>
+            <span :hidden="$vuetify.breakpoint.smAndDown">Open in new tab</span>
+          </v-btn>
+          <v-btn
+            :icon="$vuetify.breakpoint.smAndDown"
+            text
+            dark
+            color="red accent-1"
+            @click="deleteDocument(meta.identifier)"
+          >
+            <v-icon :left="!$vuetify.breakpoint.smAndDown">mdi-delete</v-icon>
+            <span :hidden="$vuetify.breakpoint.smAndDown">Delete this document</span>
+          </v-btn>
+        <!--</v-toolbar-items>-->
+      </v-toolbar>
+      <v-card-text>
+        <v-row>
+          <v-col cols="12" md="5">
+            <v-chip-group>
+              <v-chip
+                v-for="tag in meta.tags"
+                :key="tag.label"
+                label
+              >
+                <v-icon v-if="tag.label === '..'">mdi-sync</v-icon>
+                <span v-else>{{ tag.label }}</span>
+                <v-divider class="mx-3" vertical v-if="tag.parameter"></v-divider>
+                <strong v-if="tag.parameter">{{ tag.parameter.type === 'http://www.w3.org/2001/XMLSchema#decimal' ? tag.parameter.value : calculateDatestamp(tag.parameter.value) }}</strong>
+              </v-chip>
+            </v-chip-group>
+            <v-progress-linear
+              v-if="loading"
+              indeterminate
+              rounded
+              height="6"
+            />
+          </v-col>
+
+          <v-col cols="12" md="7">
+            <v-toolbar dense>
+              <v-spacer/>
+              <v-btn icon @click="scaleDown">
+                <v-icon>mdi-magnify-minus-outline</v-icon>
+              </v-btn>
+              <v-text-field
+                hide-details
+                dense
+                single-line
+                outlined
+                :value="getScale()"
+                @input="setScale"
+                clearable
+                @click:clear="scaleOff"
+                suffix="%"
+                type="number"
+                step="10"
+                class="narrow"
+              />
+              <v-btn icon @click="scaleUp">
+                <v-icon>mdi-magnify-plus-outline</v-icon>
+              </v-btn>
+
+              <!--
+              <v-btn icon disabled/>
+
+              <v-btn icon @click="page > 1 ? page-- : 1">
+                <v-icon>mdi-chevron-left</v-icon>
+              </v-btn>
+              <v-text-field
+                hide-details
+                dense
+                single-line
+                outlined
+                v-model="page"
+                :suffix="`/${ numPages ? numPages : '∞' }`"
+                type="number"
+                step="1"
+              />
+              <v-btn icon @click="page < numPages ? page++ : 1">
+                <v-icon>mdi-chevron-right</v-icon>
+              </v-btn>
+              -->
+              <v-spacer/>
+            </v-toolbar>
+
+            <pdf
+              :src="pdfsrc()"
+              v-for="i in numPages"
+              :key="i"
+              :id="i"
+              :page="i"
+              :scale.sync="scale"
+              :resize="resize"
+              @loading="loadingChange"
+              class="pdf"
+            />
+          </v-col>
+        </v-row>
       </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn
-          text
-          color="primary"
-          @click="openDocument(meta.identifier)"
-        >
-          <v-icon left>mdi-open-in-new</v-icon>
-          Open in new tab
-        </v-btn>
-        <v-btn
-          text
-          color="accent"
-          @click="deleteDocument(meta.identifier)"
-        >
-          <v-icon left>mdi-delete</v-icon>
-          Delete this document
-        </v-btn>
-        <v-btn v-if="$vuetify.breakpoint.mdAndUp" color="secondary darken-1" text @click="show = false">Close</v-btn>
-      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
@@ -103,8 +151,9 @@ export default class DocumentDetails extends Vue {
   numPages = 0
   pdfdata = undefined as undefined|Promise<any>
   errors = []
-  scale = 'page-width'
+  scale = 'page-width' as string|number
   loading = false
+  resize = true
 
   pdfsrc () {
     return {
@@ -154,6 +203,48 @@ export default class DocumentDetails extends Vue {
     }
   }
 
+  loadingChange (b: boolean) {
+    if (!b || b) {
+      const cv = document.getElementsByTagName('canvas')
+      for (let i = 0; i < cv.length; i++) {
+        cv[i].classList.add('elevation-4')
+      }
+    }
+  }
+
+  scaleDown () {
+    this.resize = false
+    this.scale = parseFloat('' + this.scale)
+    if (this.scale > 0.1) {
+      this.scale = (this.scale - 0.1).toFixed(1)
+    } else {
+      this.scale = 0.1
+    }
+  }
+
+  scaleUp () {
+    this.resize = false
+    this.scale = parseFloat('' + this.scale)
+    if (this.scale < 1.9) {
+      this.scale = (this.scale + 0.1).toFixed(1)
+    } else {
+      this.scale = 2
+    }
+  }
+
+  scaleOff () {
+    this.scale = 'page-width'
+    this.resize = true
+  }
+
+  getScale () {
+    return parseInt('' + (+this.scale * 100))
+  }
+
+  setScale (v: number) {
+    this.scale = v / 100
+  }
+
   getPdf () {
     const url = (
       (this.server().url.startsWith('https://') || this.server().url.startsWith('http://'))
@@ -169,3 +260,16 @@ export default class DocumentDetails extends Vue {
   }
 }
 </script>
+
+<style lang="scss">
+
+.narrow {
+  flex-grow: 0;
+  flex-basis: 7em;
+}
+
+.pdf {
+  width: 100%;
+  margin: 20px auto;
+}
+</style>

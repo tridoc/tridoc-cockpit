81c4d27675a08ae4ef82f14f30dd4ac793e27ef3
nleanba <noam@factsmission.com>
2021-07-29 22:16:47 +0200

    pwa?!

--- src/serviceworker/sw.ts
+++ src/serviceworker/sw.ts
@@ -1,9 +1,38 @@
 // Default type of `self` is `WorkerGlobalScope & typeof globalThis`
 // https://github.com/microsoft/TypeScript/issues/14877
 import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
 
 declare const self: ServiceWorkerGlobalScope;
 
 // Use the imported Workbox libraries to implement caching,
 // routing, and other logic:
 precacheAndRoute((self as any).__WB_MANIFEST);
+
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+registerRoute(
+  ({ url }) => url.origin === 'https://fonts.googleapis.com' || url.href === 'https://rsms.me/inter/inter.css' || url.href === 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css',
+  new StaleWhileRevalidate({
+    cacheName: 'font-stylesheets',
+  })
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+registerRoute(
+  ({ url }) => url.origin === 'https://fonts.gstatic.com' || ((url.origin === 'https://rsms.me' || url.origin === 'https://cdn.jsdelivr.net') && url.search),
+  new CacheFirst({
+    cacheName: 'font-webfonts',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
